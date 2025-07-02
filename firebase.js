a303cec25762d2415d8636353b74ae106e2776fa
ChemieAi <kzlyburak@gmail.com>
2025-07-02 22:20:07 +0300

    fix

--- firebase.js
+++ firebase.js
@@ -1,14 +1,21 @@
-// firebase.js
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/auth';
+import { 
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBiRZ1m5vjoOfdNrT2xwh5afcnvRpNwg0U",
-  authDomain: "booklibapp.firebaseapp.com",
-  projectId: "booklibapp",
-  storageBucket: "booklibapp.appspot.com",
-  messagingSenderId: "54186310472",
-  appId: "1:54186310472:android:a0e378a14209ba7dd7517d",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 if (!firebase.apps.length) {
